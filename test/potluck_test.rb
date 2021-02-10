67ed7eacffdfcdf25c9fecf5842ab039e44329da
koshea9 <keegan.oshea9@gmail.com>
2021-02-09 19:37:21 -0700

    Add test get all method

--- test/potluck_test.rb
+++ test/potluck_test.rb
@@ -29,7 +29,26 @@ class PotluckTest < Minitest::Test
     potluck.add_dish(couscous_salad)
     potluck.add_dish(cocktail_meatballs)
     potluck.dishes
-    
+
     assert_equal 2, potluck.dishes.length
+  end
+
+  def test_it_can_get_all_from_category
+    potluck = Potluck.new("7-13-18")
+    couscous_salad = Dish.new("Couscous Salad", :appetizer)
+    summer_pizza = Dish.new("Summer Pizza", :appetizer)
+    roast_pork = Dish.new("Roast Pork", :entre)
+    cocktail_meatballs = Dish.new("Cocktail Meatballs", :entre)
+    candy_salad = Dish.new("Candy Salad", :dessert)
+    potluck.add_dish(couscous_salad)
+    potluck.add_dish(summer_pizza)
+    potluck.add_dish(roast_pork)
+    potluck.add_dish(cocktail_meatballs)
+    potluck.add_dish(candy_salad)
+
+     assert_equal "Couscous Salad", potluck.get_all_from_category(:appetizer).first.name
+     assert_equal "Roast Pork", potluck.get_all_from_category(:entre).first.name
+     assert_equal "Summer Pizza", potluck.get_all_from_category(:appetizer).last.name
+
   end
 end
